beb072d23ce85b3f7e5587a583513bfba314cd4e
iiTONELOC <anthonytropeano@me.com>
2022-08-02 18:01:09 -0400

    Fixed some CSS styling issues

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -132,21 +132,21 @@ function svgIcon(svgPath, extraSvgClasses = '') {
 function historyItem(_history) {
     const historyData = [
         formatTime(_history.time),
-        _history.query
     ];
-
-    const pEl = () => historyData.map(text => `<p> ${text}</p>`).join('\n');
-
+    const renderClass=idx=>idx === 1?  'class="ind-search-query"' : '';
+    
+    const pEl = () => historyData.map((text, idx) => `<p ${renderClass(idx)}> ${text}</p>`).join('\n');
+    const pinkFocus = `class="pink-focus-outline"`;
     return (
-        `<li tabindex=0 id=${_history.time}>
+        `<li tabindex=0 id=${_history.time} ${pinkFocus}>
             ${svgIcon(`<path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" 
                 clip-rule="evenodd" />`)}
 
             <section>
-                <a href="${_history.url}" target="_blank" rel="noopener noreferrer">${pEl()}</a>
+                <a href="${_history.url}" target="_blank" rel="noopener noreferrer" ${pinkFocus}>${pEl()}</a>
             </section>
 
-            ${svgIcon(`<path tabindex=0 data-id=${_history.time} fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2
+            ${svgIcon(`<path ${pinkFocus} tabindex=0 data-id=${_history.time} fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2
                 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1
                 1 0 00-1-1z" clip-rule="evenodd"/>`, 'delete-icon')}
         </li>`
